df01a5a8110a3efbd1e934d5ff2de5bcaf1a5850
forever-bel <m2407243@edu.misis.ru>
2025-06-05 12:22:13 +0300

    Add files via upload

---
+++ CreateProduct.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.Windows;
+using Model.Data;
+using Model.Core;
+using System.Windows.Controls;
+
+namespace FoodQualityAnalyzer
+{
+    public partial class AddProductWindow : Window
+    {
+        public FoodProduct NewProduct { get; private set; }
+        public AddProductWindow()
+        {
+            InitializeComponent();
+
+            // Заполняем ComboBox типами продуктов
+            TypeComboBox.ItemsSource = new List<string>
+            {
+                "Овощ",
+                "Фрукт",
+                "Мясо",
+                "Выпечка"
+            };
+            TypeComboBox.SelectedIndex = 0;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                MessageBox.Show("Введите название продукта", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!int.TryParse(MaxTextBox.Text, out int maxExpiryDays) || maxExpiryDays <= 0)
+            {
+                MessageBox.Show("Введите корректный Максимальный срок годности (дней)", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (!int.TryParse(OstTextBox.Text, out int ostExpiryDays) || ostExpiryDays <= 0)
+            {
+                MessageBox.Show("Введите корректный Оставшийся срок годности (дней)", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (int.Parse(OstTextBox.Text) > int.Parse(MaxTextBox.Text))
+            {
+                MessageBox.Show("Оставшийся срок годности не может превышать Максимальный срок годности", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if ((!Case1.IsChecked.Value && !Case2.IsChecked.Value) && ((bool)!Case3.IsChecked && (bool)!Case4.IsChecked) &&
+               ((bool)!Case5.IsChecked && (bool)!Case6.IsChecked) && ((bool)!Case7.IsChecked && (bool)!Case8.IsChecked))
+            {
+                var result = MessageBox.Show("Проверьте корректность выбранных характеристик продукта. Хотите продолжить без выбора категории?",
+                               "Предупреждение",
+                               MessageBoxButton.YesNo,
+                               MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.No)
+                {
+                    return; // Прерываем сохранение, если пользователь выбрал "Нет"
+                }
+            }
+
+            switch (TypeComboBox.SelectedItem)
+            {
+                case "Овощ":
+                    NewProduct = new Vegetable(NameTextBox.Text, int.Parse(OstTextBox.Text), int.Parse(MaxTextBox.Text), Case1.IsChecked == true, Case2.IsChecked == true);
+                    break;
+                case "Фрукт":
+                    NewProduct = new Fruit(NameTextBox.Text, int.Parse(OstTextBox.Text), int.Parse(MaxTextBox.Text), Case3.IsChecked == true, Case4.IsChecked == true);
+                    break;
+                case "Мясо":
+                    NewProduct = new Meat(NameTextBox.Text, int.Parse(OstTextBox.Text), int.Parse(MaxTextBox.Text), Case5.IsChecked == true, Case6.IsChecked == true);
+                    break;
+                case "Выпечка":
+                    NewProduct = new Backery(NameTextBox.Text, int.Parse(OstTextBox.Text), int.Parse(MaxTextBox.Text), Case7.IsChecked == true, Case8.IsChecked == true);
+                    break;
+            }
+
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.AddProduct(NewProduct);
+
+            Model.Core.FoodQualityAnalyzer.Add(NewProduct);
+            ProductAdd_Json.SaveProduct(NewProduct);
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void TypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TypeComboBox.SelectedItem == "Овощ")
+            {
+                Case1.Visibility = Visibility.Visible;
+                Case2.Visibility = Visibility.Visible;
+                Case3.Visibility = Visibility.Collapsed;
+                Case4.Visibility = Visibility.Collapsed;
+                Case5.Visibility = Visibility.Collapsed;
+                Case6.Visibility = Visibility.Collapsed;
+                Case7.Visibility = Visibility.Collapsed;
+                Case8.Visibility = Visibility.Collapsed;
+            }
+            if (TypeComboBox.SelectedItem == "Фрукт")
+            {
+                Case1.Visibility = Visibility.Collapsed;
+                Case2.Visibility = Visibility.Collapsed;
+                Case3.Visibility = Visibility.Visible;
+                Case4.Visibility = Visibility.Visible;
+                Case5.Visibility = Visibility.Collapsed;
+                Case6.Visibility = Visibility.Collapsed;
+                Case7.Visibility = Visibility.Collapsed;
+                Case8.Visibility = Visibility.Collapsed;
+            }
+            if (TypeComboBox.SelectedItem == "Мясо")
+            {
+                Case1.Visibility = Visibility.Collapsed;
+                Case2.Visibility = Visibility.Collapsed;
+                Case3.Visibility = Visibility.Collapsed;
+                Case4.Visibility = Visibility.Collapsed;
+                Case5.Visibility = Visibility.Visible;
+                Case6.Visibility = Visibility.Visible;
+                Case7.Visibility = Visibility.Collapsed;
+                Case8.Visibility = Visibility.Collapsed;
+            }
+            if (TypeComboBox.SelectedItem == "Выпечка")
+            {
+                Case1.Visibility = Visibility.Collapsed;
+                Case2.Visibility = Visibility.Collapsed;
+                Case3.Visibility = Visibility.Collapsed;
+                Case4.Visibility = Visibility.Collapsed;
+                Case5.Visibility = Visibility.Collapsed;
+                Case6.Visibility = Visibility.Collapsed;
+                Case7.Visibility = Visibility.Visible;
+                Case8.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Model.Core;
+using Model.Data;
+using System.IO;
+
+namespace FoodQualityAnalyzer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void UpdateAnalyzeButtonState()
+        {
+            AnalyzeButton.IsEnabled = VegetablePanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    FruitPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    MeatPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    BackeryPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true);
+        }
+
+        private void AnalyzeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedProducts = new FoodProduct[0];
+
+            foreach (var checkBox in FindVisualChildren<CheckBox>(this))
+            {
+                if (checkBox.IsChecked == true)
+                {
+                    foreach (var p in Model.Core.FoodQualityAnalyzer.Products)
+                    {
+                        if (p.Name == checkBox.Content.ToString())
+                        {
+                            Array.Resize(ref selectedProducts, selectedProducts.Length + 1);
+                            selectedProducts[selectedProducts.Length - 1] = p;
+                        }
+                    }
+                }
+            }
+
+            if (selectedProducts.Length > 0)
+            {
+                var chartWindow = new ChartWindow(selectedProducts, this);
+                chartWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Выберите хотя бы один продукт", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        public CheckBox FindCheckBox(string content)
+        {
+            foreach (var child in FindVisualChildren<CheckBox>(this))
+            {
+                if (child.Content?.ToString() == content)
+                    return child;
+            }
+            return null;
+        }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProductAdd_Json.LoadProducts();
+            foreach (var p in Model.Core.FoodQualityAnalyzer.Products)
+            {
+                AddProduct(p);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var add = new AddProductWindow();
+            add.Show();
+        }
+
+        public void AddProduct(FoodProduct product)
+        {
+            CheckBox NewCheckBox = new CheckBox()
+            {
+                Content = product.Name,
+                Margin = new Thickness(20, 2, 0, 2),
+                IsChecked = false
+            };
+
+            NewCheckBox.Checked += (s, e) => UpdateAnalyzeButtonState();
+            NewCheckBox.Unchecked += (s, e) => UpdateAnalyzeButtonState();
+
+            NewCheckBox.Checked += (s, e) => UpdateDeleteButtonState();
+            NewCheckBox.Unchecked += (s, e) => UpdateDeleteButtonState();
+
+            if (product is Vegetable) VegetablePanel.Children.Add(NewCheckBox);
+            if (product is Fruit) FruitPanel.Children.Add(NewCheckBox);
+            if (product is Meat) MeatPanel.Children.Add(NewCheckBox);
+            if (product is Backery) BackeryPanel.Children.Add(NewCheckBox);
+        }
+
+        private void UpdateDeleteButtonState()
+        {
+            DeleteButton.IsEnabled = VegetablePanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    FruitPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    MeatPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true) ||
+                                    BackeryPanel.Children.OfType<CheckBox>().Any(c => c.IsChecked == true);
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Уверены, что хотите удалить выбранные продукты?",
+                   "Предупреждение",
+                   MessageBoxButton.YesNo,
+                   MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            var prods = new List<FoodProduct>();
+            var cb = new List<CheckBox>();
+            foreach (var checkBox in FindVisualChildren<CheckBox>(this))
+            {
+                if (checkBox.IsChecked == true)
+                {
+                    foreach (var p in Model.Core.FoodQualityAnalyzer.Products)
+                    {
+                        if (p.Name == checkBox.Content.ToString())
+                        {
+                            var filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ProductsJson", p.Name + ".json");
+                            File.Delete(filepath);
+
+                            filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ProductsXML", p.Name + ".xml");
+                            File.Delete(filepath);
+
+                            prods.Add(p);
+                            cb.Add(checkBox);
+
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            for (int i = 0; i < prods.Count; i++)
+            {
+                var p = prods[i];
+                Model.Core.FoodQualityAnalyzer.Remove(p);
+                cb[i].IsChecked = false;
+                if (p is Vegetable) VegetablePanel.Children.Remove(cb[i]);
+                if (p is Fruit) FruitPanel.Children.Remove(cb[i]);
+                if (p is Meat) MeatPanel.Children.Remove(cb[i]);
+                if (p is Backery) BackeryPanel.Children.Remove(cb[i]);
+            }
+        }
+    }
+}
